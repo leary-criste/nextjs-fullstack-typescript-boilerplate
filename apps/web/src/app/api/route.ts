031c4b476f18fdcdab3fd960cc785326063ca42f
leary-criste <leary.criste@outlook.com>
2026-04-07 01:24:07 -0300

    delete format route.ts

--- apps/web/src/app/api/route.ts
+++ apps/web/src/app/api/route.ts
@@ -4,4 +4,4 @@ import sendRes from '@server/_/functions/sendRes';
 import type { Route } from '@server/_/types';
 
 export const GET: Route = async () =>
-  sendRes(undefined, constants.SUCCESS, { message: constants.API_HANDSHAKE });
+  sendRes(undefined, constants.SUCCESS, { message: constants.API_HANDSHAKE });
