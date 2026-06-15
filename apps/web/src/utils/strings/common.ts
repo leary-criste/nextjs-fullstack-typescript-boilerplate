80ec54928dbbba2dd590a119672f9bad687b935d
leary-criste <leary.criste@outlook.com>
2026-06-15 02:45:28 -0300

    delete format common.ts

--- apps/web/src/utils/strings/common.ts
+++ apps/web/src/utils/strings/common.ts
@@ -7,4 +7,4 @@ export const escapeRegex: EscapeRegex = (str) =>
 // Capitalizes the first letter of a string
 export type Capitalize = (str: string) => string;
 
-export const capitalize: Capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+export const capitalize: Capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
